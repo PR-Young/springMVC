dd50e918e741b6ea2b1eceea1e39efc5ab95c59a
yngamo <1065569578@qq.com>
2018-07-02 16:04:28 +0800

    新增mybatis，日志，登录页面

---
+++ demo/src/main/java/cn/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package cn.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.demo.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private IUserService userService;
+
+	/*
+	 * 用户列表与分页Action
+	 */
+	@RequestMapping("/list")
+	public String list(Model model, @RequestParam(required = false, defaultValue = "1") int pageNO) {
+		int size = 5;
+		model.addAttribute("size", size);
+		model.addAttribute("pageNO", pageNO);
+		model.addAttribute("count", userService.queryUserCount());
+		model.addAttribute("userList", userService.queryUserPager(pageNO, size));
+		// return "user/list";
+		return "demo";
+	}
+}
